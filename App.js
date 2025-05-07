c28f5dc10d3a5ef12e268d349ee546d0e09c50e7
AfiyaZannuba <afiya.zannubaa@gmail.com>
2025-05-07 23:27:57 +0530

    episode-1 and 2 completed

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
-const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "h1tag"}, "I'm Afiya"), React.createElement("p", {id:"ptag"}, "how are you?")]));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "h1tag", key: "heading" }, "I'm Afiya"),
+    React.createElement("h6", { id: "ptag", key: "paragraph" }, "how are you?")
+  ])
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render(parent);

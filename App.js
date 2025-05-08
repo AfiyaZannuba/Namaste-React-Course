cb15593976bae277e267fa75a807e16607dbd4e0
AfiyaZannuba <afiya.zannubaa@gmail.com>
2025-05-08 16:45:06 +0530

    Episode-3 Component composition problem in App.js

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "h1tag", key: "heading" }, "I'm Afiya"),
-    React.createElement("h6", { id: "ptag", key: "paragraph" }, "how are you?")
-  ])
-);
+const Title = () => {
+  return(
+  <h1 className="head">
+    Namaste React using JSX
+  </h1>
+  )
+}
+
+const HeadingComponent = () => {
+  return(
+    <>
+    <Title />
+    <h1>Namaste React Functional Component</h1>
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
